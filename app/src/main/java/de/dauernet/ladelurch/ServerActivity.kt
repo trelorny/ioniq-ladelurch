56ff395f42c294132897ccfdb02173525b75d004
Tim Dauer <tim.dauer@rewe-digital.com>
2018-05-28 14:03:32 +0200

    Initial Creation of Android project

---
+++ app/src/main/java/de/dauernet/ladelurch/ServerActivity.kt
@@ -0,0 +1,12 @@
+package de.dauernet.ladelurch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ServerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server)
+    }
+}
